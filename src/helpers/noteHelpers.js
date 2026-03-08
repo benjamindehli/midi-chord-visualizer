74f73e9fc63838d8ae46c237fa469367dfd60567
Benjamin Dehli <superelg@gmail.com>
2026-03-08 15:40:07 +0100

    Update midi-notes-to-chord
    v1.0.7

--- src/helpers/noteHelpers.js
+++
@@ -1,7 +0,0 @@
-export function getRelativeNoteNumber(noteNumber, keyNoteNumber) {
-    let relativeNumber = noteNumber - keyNoteNumber;
-    if (relativeNumber < 0) {
-        relativeNumber += 12;
-    }
-    return relativeNumber;
-}
